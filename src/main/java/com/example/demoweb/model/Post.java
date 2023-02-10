b4b1893a6622dd40343e312b3eac29494b28969d
YesNile <holmovdanil2@gmail.com>
2023-02-10 22:02:52 +0500

    Full 1 module Complete

---
+++ src/main/java/com/example/demoweb/model/Post.java
@@ -0,0 +1,19 @@
+package com.example.demoweb.model;
+
+public class Post {
+
+    private final String text;
+    private Integer likes;
+
+    public Post(String text) {
+        this.text = text;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public Integer getLikes() {
+        return likes;
+    }
+}
