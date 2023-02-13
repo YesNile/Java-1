213cadb84568d31c27fad777db05b97e2b680f3b
YesNile <holmovdanil2@gmail.com>
2023-02-13 14:12:17 +0500

    7.5 Complete

--- src/main/java/com/example/demoweb/service/PostService.java
+++ src/main/java/com/example/demoweb/service/PostService.java
@@ -5,15 +5,23 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 @Service
 public class PostService {
 
-    public ArrayList<Post> listAllPosts(){
-        ArrayList<Post> list = new ArrayList<>();
-        list.add(new Post("text 1", new Date()));
-        list.add(new Post("text 2", new Date()));
-        list.add(new Post("text 3", new Date()));
-        return list;
+    List<Post> posts = new ArrayList<>();
+
+    public List<Post> listAllPosts(){
+        return posts;
+    }
+
+    {
+        posts.add(new Post("202", new Date()));
+        posts.add(new Post("101", new Date()));
+        posts.add(new Post("tetet",new Date()));
+    }
+    public void create(String text) {
+        posts.add(new Post(text, new Date()));
     }
 }
