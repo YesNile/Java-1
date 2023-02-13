e17f7915bb314d6e2129709b5f1e696c8e7e6496
YesNile <holmovdanil2@gmail.com>
2023-02-13 13:45:05 +0500

    6.2 Complete

--- src/main/java/com/example/demoweb/model/Post.java
+++ src/main/java/com/example/demoweb/model/Post.java
@@ -1,19 +1,27 @@
 package com.example.demoweb.model;
 
+import java.util.Date;
+
 public class Post {
 
     private final String text;
-    private Integer likes;
+    private int likes;
 
-    public Post(String text) {
+    private Date creationDate;
+
+    public Post(String text, Date creationDate) {
         this.text = text;
+        this.creationDate = creationDate;
     }
 
     public String getText() {
         return text;
     }
 
-    public Integer getLikes() {
+    public int getLikes() {
         return likes;
+    }
+    public Date getCreationDate() {
+        return creationDate;
     }
 }
